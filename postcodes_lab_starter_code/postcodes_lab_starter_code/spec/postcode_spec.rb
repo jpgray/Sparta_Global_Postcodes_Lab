b897cba3d1d0547a5d872f9e209217537beb1320
Jon Gray <jgray@spartaglobal.com>
2018-11-27 14:51:48 +0000

    first attempt at all tests, looking to refactor

--- postcodes_lab_starter_code/postcodes_lab_starter_code/spec/postcode_spec.rb
+++ postcodes_lab_starter_code/postcodes_lab_starter_code/spec/postcode_spec.rb
@@ -30,6 +30,10 @@ describe Postcodesio do
       expect(@postcodesio.get_result(@response)["eastings"]).to be_kind_of Integer
     end
 
+    it "should return an ordnance survey northings value as integer" do
+      expect(@postcodesio.get_result(@response)["northings"]).to be_kind_of Integer
+    end
+
     it "should return a country which is one of the four constituent countries of the UK" do
       expect(@postcodesio.get_result(@response)["country"]).to eq("England").or eq("Scotland").or eq("Wales").or eq("Northern Ireland")
     end
@@ -95,6 +99,7 @@ describe Postcodesio do
     before(:all) do
       @postcodesio = Postcodesio.new
       @response = @postcodesio.get_multiple_postcodes(['KT19 8JG', 'KT18 2DJ']) #Add in array of postcodes
+      # to check a different set of result than the first, change the "[0]" below to the appropriate index
       @response_result_to_check = @postcodesio.get_result(@response)[0]["result"]
     end
 
@@ -127,117 +132,72 @@ describe Postcodesio do
       end
 
       it "should return an ordnance survey eastings value as integer" do
+        expect(@response_result_to_check["eastings"]).to be_kind_of Integer
       end
 
       it "should return an ordnance survey eastings value as integer" do
+        expect(@response_result_to_check["northings"]).to be_kind_of Integer
       end
 
       it "should return a country which is one of the four constituent countries of the UK" do
+        expect(@response_result_to_check["country"]).to eq("England").or eq("Scotland").or eq("Wales").or eq("Northern Ireland")
       end
 
       it "should return a string value for NHS authority " do
+        expect(@response_result_to_check["nhs_ha"]).to be_kind_of String
       end
 
       it "should return a longitude float value" do
+        expect(@response_result_to_check["longitude"]).to be_kind_of Float
       end
 
       it "should return a latitude float value" do
+        expect(@response_result_to_check["latitude"]).to be_kind_of Float
       end
 
       it "should return a parliamentary constituency string" do
+        expect(@response_result_to_check["parliamentary_constituency"]).to be_kind_of String
       end
 
       it "should return a european_electoral_region string" do
+        expect(@response_result_to_check["european_electoral_region"]).to be_kind_of String
       end
 
       it "should return a primary_care_trust string" do
+        expect(@response_result_to_check["primary_care_trust"]).to be_kind_of String
       end
 
       it "should return a region string" do
+        expect(@response_result_to_check["region"]).to be_kind_of String
       end
 
       it "should return a parish string" do
+        expect(@response_result_to_check["parish"]).to be_kind_of String
       end
 
       it "should return a lsoa string" do
+        expect(@response_result_to_check["lsoa"]).to be_kind_of String
       end
 
       it "should return a msoa string" do
+        expect(@response_result_to_check["msoa"]).to be_kind_of String
       end
       # admin ward and county are not documented however tested below
 
       it "should return a admin_district string" do
+        expect(@response_result_to_check["admin_district"]).to be_kind_of String
       end
 
       it "should return a incode string of three characters" do
-      end
-
-      it "should return a msoa string" do
-      end
-
-      it "should return a incode string of 3-4 characters" do
-      end
-
-      it "should have a results hash" do
-      end
-
-      it "should return a postcode between 5-7 in length"  do
-      end
-
-      it "should return an quality key integer between 1-9" do
-      end
-
-      it "should return an ordnance survey eastings value as integer" do
-      end
-
-      it "should return an ordnance survey eastings value as integer" do
-      end
-
-      it "should return a country which is one of the four constituent countries of the UK" do
-      end
-
-      it "should return a string value for NHS authority " do
-      end
-
-      it "should return a longitude float value" do
-      end
-
-      it "should return a latitude float value" do
-      end
-
-      it "should return a parliamentary constituency string" do
-      end
-
-      it "should return a european_electoral_region string" do
-      end
-
-      it "should return a primary_care_trust string" do
-      end
-
-      it "should return a region string" do
-      end
-
-      it "should return a parish string" do
-      end
-
-      it "should return a lsoa string" do
-      end
-
-      it "should return a msoa string" do
-      end
-      # admin ward and county are not documented however tested below
-
-      it "should return a admin_district string" do
-      end
-
-      it "should return a incode string of three characters" do
-      end
-
-      it "should return a msoa string" do
+        expect(@response_result_to_check["incode"]).to be_kind_of String
+        expect(@response_result_to_check["incode"].length).to eq 3
       end
 
       it "should return a incode string of 3-4 characters" do
+        expect(@response_result_to_check["incode"]).to be_kind_of String
+        expect(@response_result_to_check["incode"].length).to eq 3.or eq 4
       end
+
     end
   end
 
